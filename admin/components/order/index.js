d7877e2d93603e8bada1bc7b1a1715db9c8fb448
siam chowdhury <smsiam696@gmail.com>
2023-11-29 17:03:20 +0600

    update

--- admin/components/order/index.js
+++ admin/components/order/index.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, {useEffect} from "react";
 import SearchOrder from "./SearchOrder";
 import OrderTable from "../placeOrder/OrderTable";
+import { db } from "@/app/utils/firebase";
+import { useDispatch } from "react-redux";
+import { updateOrder } from "@/app/redux/slices/orderSlice";
 
 const Order = () => {
+const dispatch = useDispatch();
+    // Get order from firebase database
+    useEffect(() => {
+
+      const unSub = db
+        .collection("placeOrder")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snap) => {
+          const order = [];
+          snap.docs.map((doc) => {
+            order.push({
+              id: doc.id,
+              ...doc.data(),
+              // timestamp: doc.data().timestamp?.toDate().getTime(),
+            });
+          });
+          dispatch(updateOrder(order));
+     
+        });
+      return () => {
+        unSub();
+      };
+    }, []);
+
   return (
     <main>
       <div className="grid mx-auto">
